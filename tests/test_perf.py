380c6c8fd6a66015eb428e756f51a31b24c21ee0
Valery Chernov <valery.chernov@deelvin.com>
2022-08-31 14:11:06 +0300

    add perf test to compare pure ORT and ORT with TVM in custom op

---
+++ tests/test_perf.py
@@ -0,0 +1,54 @@
+import argparse
+from functools import partial
+import logging
+
+import tempfile
+
+from test_utils import (
+    unpack_onnx_tar,
+    generate_input_shapes_dtypes,
+    generate_input_data,
+    get_ort_inference_session,
+    perf_test,
+)
+
+logging.basicConfig(level=logging.CRITICAL)
+
+
+def test_benchmark(
+    model_path,
+):
+    benchmark_test = partial(perf_test, iters_number = 1000, model_name = "test model")
+    with tempfile.TemporaryDirectory() as tmp:
+        onnx_path, custom_op_libs = unpack_onnx_tar(model_path, tmp)
+
+        input_shapes, input_dtypes = generate_input_shapes_dtypes(onnx_path)
+        input_data = generate_input_data(input_shapes=input_shapes, input_dtypes=input_dtypes)
+        
+        # ORT with custom op
+        engine = get_ort_inference_session(onnx_path, custom_op_libs)
+        ort_runner = partial(engine.run, output_names=None, input_feed=input_data)
+
+        benchmark_test(ort_runner, framework_name = "ONNX Runtime with Custom Op")
+        
+        # pure ORT
+        engine = get_ort_inference_session(onnx_path)
+        ort_runner = partial(engine.run, output_names=None, input_feed=input_data)
+
+        benchmark_test(ort_runner, framework_name = "Pure ONNX Runtime")
+
+
+def main():  # pragma: no cover
+    parser = argparse.ArgumentParser(description="Performance test.")
+    parser.add_argument(
+        "--model",
+        required=True,
+        help="Path to the Model to tune (in ONNX format).",
+    )
+    args = parser.parse_args()
+
+    test_benchmark(model_path=args.model)
+
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
---
+++ tests/test_utils.py
@@ -0,0 +1,93 @@
+"""
+Utilities for tests
+"""
+
+import os
+import re
+import numpy as np
+import time
+
+import tarfile
+import onnx
+import onnxruntime
+
+
+def find(pattern, path):
+    result = []
+    regex = re.compile(pattern)
+    for root, _, files in os.walk(path):
+        for name in files:
+            if regex.match(name):
+                result.append(os.path.join(root, name))
+    return result
+
+def unpack_onnx_tar(model_path, tmp_dir):
+    with tarfile.open(model_path, "r") as tar:
+      tar.extractall(tmp_dir)
+    files = find(".*\\.onnx$", tmp_dir)
+    if len(files) < 1:
+        print("No onnx model found")
+        exit(-1)
+    elif len(files) > 1:
+        print("Multiple onnx models found")
+        exit(-1)
+    onnx_path = files[0]
+    custom_op_libs_paths = find("^custom_.*\\.(so|dll|dynlib)$", tmp_dir)
+    
+    return onnx_path, custom_op_libs_paths
+
+def generate_input_shapes_dtypes(onnx_model_path):
+    onnx_model = onnx.load_model(onnx_model_path)
+    input_shapes = {}
+    input_dtypes = {}
+    for inp in onnx_model.graph.input:
+        name = inp.name
+        shape = [d.dim_value for d in inp.type.tensor_type.shape.dim]
+        input_shapes[name] = shape
+        dtype = inp.type.tensor_type.elem_type
+        input_dtypes[name] = onnx.mapping.TENSOR_TYPE_TO_NP_TYPE[dtype]
+
+    return input_shapes, input_dtypes
+
+def generate_input_data(
+    input_shapes,
+    input_dtypes,
+):
+    data = {}
+    for name in input_shapes.keys():
+        shape = input_shapes[name]
+        dtype = input_dtypes[name]
+        high_val = 1.0
+        if (dtype == "int32" or dtype == "int64"):
+            high_val = 1000.0
+        d = np.random.uniform(size=shape, high=high_val).astype(dtype)
+        data[name] = d
+    return data
+
+def get_ort_inference_session(onnx_path, custom_op_libs = None):
+    sess_options = onnxruntime.SessionOptions()
+    if custom_op_libs:
+        for custom_op_lib in custom_op_libs:
+            sess_options.register_custom_ops_library(custom_op_lib)
+
+    engine = onnxruntime.InferenceSession(
+        onnx_path,
+        providers=["CPUExecutionProvider"],
+        provider_options=[{}],
+        sess_options=sess_options,
+    )
+    return engine
+
+def perf_test(run, iters_number = 1000, model_name = "ResNet50-v1", framework_name = "ort+tvm", pre_iters_num = 5):
+    assert iters_number > 0
+
+    # warmup run
+    for i in range(pre_iters_num):
+        run()
+
+    tic = time.perf_counter()
+    for i in range(iters_number):
+        run()
+    toc = time.perf_counter()
+    dur_ms = 1000*(toc - tic) / iters_number
+    print(f"Averaged time: {dur_ms:0.4f} milliseconds for {iters_number} iterations of inference of {model_name} model by {framework_name}")
