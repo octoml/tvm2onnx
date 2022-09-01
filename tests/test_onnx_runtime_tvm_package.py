0c4842ac0802b9614e758844b75958fa3abe7ec4
KJlaccHoeUM9l <wotpricol@mail.ru>
2022-09-01 12:00:55 +0300

    add test for models with static shapes

--- tests/test_onnx_runtime_tvm_package.py
+++ tests/test_onnx_runtime_tvm_package.py
@@ -1,5 +1,6 @@
 """Tests ONNX Packaging."""
 import os
+import pathlib
 import tarfile
 import tempfile
 import shutil
@@ -7,6 +8,7 @@ import shutil
 import numpy as np
 import onnx
 import onnxruntime
+from onnx import ModelProto, mapping
 from onnx import TensorProto
 from onnx.external_data_helper import convert_model_to_external_data
 from onnx.helper import (
@@ -18,7 +20,80 @@ from onnx.helper import (
 )
 from tvm2onnx.onnx_model import ONNXModel
 
+from typing import AnyStr, Dict, List
+
 _MODEL_PATH = os.path.join(os.path.dirname(__file__), "testdata/abtest.onnx")
+
+
+def get_input_data_for_model_with_fixed_shapes(onnx_model: ModelProto) -> Dict[AnyStr, np.ndarray]:
+    """
+    Create input data for model with static shapes
+    """
+
+    def get_onnx_input_names(model: ModelProto) -> List[AnyStr]:
+        inputs = [node.name for node in model.graph.input]
+        initializer = [node.name for node in model.graph.initializer]
+        inputs = list(set(inputs) - set(initializer))
+        return sorted(inputs)
+
+    def get_onnx_input_types(model: ModelProto) -> List[np.dtype]:
+        input_names = get_onnx_input_names(model)
+        return [
+            mapping.TENSOR_TYPE_TO_NP_TYPE[node.type.tensor_type.elem_type]
+            for node in sorted(model.graph.input, key=lambda node: node.name)
+            if node.name in input_names
+        ]
+
+    def get_onnx_input_shapes(model: ModelProto) -> List[List[int]]:
+        input_names = get_onnx_input_names(model)
+        return [
+            [dv.dim_value for dv in node.type.tensor_type.shape.dim]
+            for node in sorted(model.graph.input, key=lambda node: node.name)
+            if node.name in input_names
+        ]
+
+    input_names = get_onnx_input_names(onnx_model)
+    input_shapes = get_onnx_input_shapes(onnx_model)
+    input_types = get_onnx_input_types(onnx_model)
+    assert len(input_names) == len(input_types) == len(input_shapes)
+    random_inputs = [np.random.uniform(size=shape).astype(dtype) for shape, dtype in zip(input_shapes, input_types)]
+    return dict(zip(input_names, random_inputs))
+
+
+def test_mnist():
+    with tempfile.TemporaryDirectory() as tdir:
+        from tvm2onnx import TVM2ONNX_PACKAGE_ROOT_DIR
+        project_root = os.path.split(TVM2ONNX_PACKAGE_ROOT_DIR)[0]
+        model_path = os.path.join(project_root, "tests", "testdata", "mnist.onnx")
+        # model_path = os.path.join(project_root, "tests", "testdata", "big_model.onnx")
+
+        source_model = ONNXModel.from_file(pathlib.Path(model_path))
+        source_model.infer_and_update_inputs()
+        relay_model = source_model.to_relay()
+        onnx_path = os.path.join(tdir, "test_model.tvm.onnx")
+        relay_model.package_to_onnx(
+            name="test_model",
+            tvm_target="llvm",
+            output_path=onnx_path,
+        )
+        model_dir = os.path.join(tdir, "model")
+        with tarfile.open(onnx_path, "r") as tar:
+            tar.extractall(model_dir)
+        onnx_model_path = os.path.join(model_dir, "test_model.onnx")
+        custom_lib = os.path.join(model_dir, "custom_test_model.so")
+
+        sess_options = onnxruntime.SessionOptions()
+        sess_options.register_custom_ops_library(custom_lib)
+
+        engine = onnxruntime.InferenceSession(
+            onnx_model_path,
+            providers=["CPUExecutionProvider"],
+            provider_options=[{}],
+            sess_options=sess_options,
+        )
+
+        output_data = engine.run(output_names=None, input_feed=get_input_data_for_model_with_fixed_shapes(onnx.load(model_path)))
+        print(output_data)
 
 
 def test_onnx_package():
@@ -172,3 +247,11 @@ def test_constant_model():
         expected = (input_data["a"] + c1_data) * c2_data
         actual = result[0]
         assert np.allclose(expected, actual)
+
+
+def main():
+    test_mnist()
+
+
+if __name__ == '__main__':
+    main()
