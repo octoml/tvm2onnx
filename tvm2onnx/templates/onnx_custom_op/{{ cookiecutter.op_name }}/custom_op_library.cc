8ba728ff5a905d298b61891be18061213c979878
Valery Chernov <valery.chernov@deelvin.com>
2022-10-26 21:00:48 +0300

    use_zero_copy key was added to custom op and two run pipelines were implemented

--- tvm2onnx/templates/onnx_custom_op/{{ cookiecutter.op_name }}/custom_op_library.cc
+++ tvm2onnx/templates/onnx_custom_op/{{ cookiecutter.op_name }}/custom_op_library.cc
@@ -88,14 +88,11 @@ class TVMRunnerZeroCopy;
 
 class TVMRunnerBase {
  public:
-  TVMRunnerBase(const Ort::CustomOpApi& ort, TVMFuncsPtr pfuncs) :
-    ort_(ort),
+  TVMRunnerBase(TVMFuncsPtr pfuncs) :
     funcs(std::move(pfuncs)) {}
   virtual ~TVMRunnerBase() = default;
 
-  // TODO(agladyshev): after PR#13215 we should use const Ort::KernelContext&
-  // Ort::KernelContext ctx(context);
-  virtual void run(OrtKernelContext* context) = 0;
+  virtual void run(const Ort::KernelContext& ctx) = 0;
 
   static DLDataType GetDataType(const ONNXTensorElementDataType& type) {
     static std::unordered_map<ONNXTensorElementDataType, DLDataType> ortTypeToDLType = {
@@ -119,7 +116,31 @@ class TVMRunnerBase {
     return ortTypeToDLType[type];
   }
 
-  Ort::CustomOpApi ort_;
+  template<typename TensorType>
+  void SetInputTensors(std::vector<TensorType>& inputs,
+                       const std::string& func_name) {
+    // arity is num of inputs + 1, because first argument to the set_input_func
+    // is the name of the function that should take those inputs.
+    size_t arity = inputs.size() + 1;
+    std::vector<TVMValue> values(arity);
+    std::vector<int> codes(arity);
+    tvm::runtime::TVMArgsSetter setter(values.data(), codes.data());
+    setter(0, func_name.c_str());
+    for (size_t k = 0; k < arity - 1; ++k) {
+    setter(k+1, &inputs[k]);
+    }
+
+    tvm::runtime::TVMRetValue rv;
+    funcs->set_input_func.CallPacked(tvm::runtime::TVMArgs(values.data(), codes.data(), arity), &rv);
+  }
+
+  static std::unique_ptr<TVMRunnerBase> GetRunner(TVMFuncsPtr pfuncs, bool use_zero_copy) {
+    if (use_zero_copy) {
+      return std::make_unique<TVMRunnerZeroCopy>(std::move(pfuncs));
+    } else {
+      return std::make_unique<TVMRunnerCopy>(std::move(pfuncs));
+    }
+  }
   TVMFuncsPtr funcs;
   // TODO(vvchernov): define device type for specific case. define device id
   DLDevice dl_device = {DLDeviceType::{{ cookiecutter.dl_device_type }}, 0};
@@ -127,17 +148,17 @@ class TVMRunnerBase {
 
 class TVMRunnerCopy : public TVMRunnerBase {
  public:
-  TVMRunnerCopy(const Ort::CustomOpApi& ort, TVMFuncsPtr pfuncs) :
-    TVMRunnerBase(ort, std::move(pfuncs)) {}
+  TVMRunnerCopy(TVMFuncsPtr pfuncs) :
+    TVMRunnerBase(std::move(pfuncs)) {}
 
-  void run(OrtKernelContext* context) final {
-    std::vector<tvm::runtime::NDArray> input_vec = GetInputTensors(context);
+  void run(const Ort::KernelContext& ctx) final {
+    std::vector<tvm::runtime::NDArray> input_vec = GetInputTensors(ctx);
     SetInputTensors(input_vec, "main");
 
     {% for details in cookiecutter.outputs -%}
-    int64_t output{{details.index}}_shape[] = {{details.shape}};
-    OrtValue* output{{details.index}} = ort_.KernelContext_GetOutput(context, {{details.index}}, output{{details.index}}_shape, {{details.rank}});
-    {{details.cpp_type}}* output{{details.index}}_ptr = ort_.GetTensorMutableData<{{details.cpp_type}}>(output{{details.index}});
+    std::vector<int64_t> output{{details.index}}_shape = {{details.shape}};
+    auto output{{details.index}} = ctx.GetOutput({{details.index}}, output{{details.index}}_shape);
+    {{details.cpp_type}}* output{{details.index}}_ptr = output{{details.index}}.GetTensorMutableData<{{details.cpp_type}}>();
     {% endfor %}
 
     tvm::runtime::ObjectRef out = funcs->run_func("main");
@@ -150,30 +171,13 @@ class TVMRunnerCopy : public TVMRunnerBase {
   }
 
  private:
-  void SetInputTensors(std::vector<tvm::runtime::NDArray>& inputs, const std::string& func_name) {
-    // arity is num of inputs + 1, because first argument to the set_input_func
-    // is the name of the function that should take those inputs.
-    size_t arity = inputs.size() + 1;
-    std::vector<TVMValue> values(arity);
-    std::vector<int> codes(arity);
-    tvm::runtime::TVMArgsSetter setter(values.data(), codes.data());
-
-    setter(0, func_name.c_str());
-    for (size_t k = 0; k < arity - 1; ++k) {
-      setter(k+1, inputs[k]);
-    }
-
-    tvm::runtime::TVMRetValue rv;
-    funcs->set_input_func.CallPacked(tvm::runtime::TVMArgs(values.data(), codes.data(), arity), &rv);
-  }
-
-  std::vector<tvm::runtime::NDArray> GetInputTensors(OrtKernelContext* context) {
+  std::vector<tvm::runtime::NDArray> GetInputTensors(const Ort::KernelContext& ctx) {
     std::vector<tvm::runtime::NDArray> input_vec;
     {% for details in cookiecutter.inputs -%}
-    const OrtValue* input{{details.index}} = ort_.KernelContext_GetInput(context, {{details.index}});
-    const {{details.cpp_type}}* input{{details.index}}_ptr = ort_.GetTensorData<{{details.cpp_type}}>(input{{details.index}});
+    auto input_tensor{{details.index}} = ctx.GetInput({{details.index}});
+    const {{details.cpp_type}}* input{{details.index}}_ptr = input_tensor{{details.index}}.GetTensorData<{{details.cpp_type}}>();
     DLDataType input{{details.index}}_dtype = tvm::runtime::String2DLDataType("{{details.numpy_dtype}}");
-    tvm::runtime::NDArray input{{details.index}}_ndarray = tvm::runtime::NDArray::Empty({{details.shape}}, input{{details.index}}_dtype, dl_device);
+    tvm::runtime::NDArray input{{details.index}}_ndarray = ::tvm::runtime::NDArray::Empty({{details.shape}}, input{{details.index}}_dtype, dl_device_type);
     input{{details.index}}_ndarray.CopyFromBytes(input{{details.index}}_ptr, {{details.element_count}}*sizeof({{details.cpp_type}}));
     input_vec.push_back(input{{details.index}}_ndarray);
     {% endfor %}
@@ -198,17 +202,17 @@ class TVMRunnerCopy : public TVMRunnerBase {
 
 class TVMRunnerZeroCopy : public TVMRunnerBase {
  public:
-  TVMRunnerZeroCopy(const Ort::CustomOpApi& ort, TVMFuncsPtr pfuncs) :
-    TVMRunnerBase(ort, std::move(pfuncs)) {}
+  TVMRunnerZeroCopy(TVMFuncsPtr pfuncs) :
+    TVMRunnerBase(std::move(pfuncs)) {}
 
-  void run(OrtKernelContext* context) final {
+  void run(const Ort::KernelContext& ctx) final {
     // Formally we should do set_input, set_outputs and run for the same func name
     // TODO(vvchernov): can func_name be not "main"? I have never seen such case
     const std::string func_name = "main";
-    std::vector<DLTensor> ort_dl_inputs = GetInputDLTensors(context);
+    std::vector<DLTensor> ort_dl_inputs = GetInputDLTensors(ctx);
     SetInputTensors(ort_dl_inputs, func_name);
 
-    std::vector<DLTensor> ort_dl_outputs = GetOutputDLTensors(context);
+    std::vector<DLTensor> ort_dl_outputs = GetOutputDLTensors(ctx);
     LinkOutputTensors(ort_dl_outputs, func_name);
 
     // Inference
@@ -216,29 +220,12 @@ class TVMRunnerZeroCopy : public TVMRunnerBase {
   }
 
  private:
-  void SetInputTensors(std::vector<DLTensor>& inputs, const std::string& func_name) {
-    // arity is num of inputs + 1, because first argument to the set_input_func
-    // is the name of the function that should take those inputs.
-    size_t arity = inputs.size() + 1;
-    std::vector<TVMValue> values(arity);
-    std::vector<int> codes(arity);
-    tvm::runtime::TVMArgsSetter setter(values.data(), codes.data());
-
-    setter(0, func_name.c_str());
-    for (size_t k = 0; k < arity - 1; ++k) {
-      setter(k+1, &inputs[k]);
-    }
-
-    tvm::runtime::TVMRetValue rv;
-    funcs->set_input_func.CallPacked(tvm::runtime::TVMArgs(values.data(), codes.data(), arity), &rv);
-  }
-
-  std::vector<DLTensor> GetInputDLTensors(OrtKernelContext* context) {
+  std::vector<DLTensor> GetInputDLTensors(const Ort::KernelContext& ctx) {
     std::vector<DLTensor> ort_dl_inputs;
     {% for details in cookiecutter.inputs -%}
-    auto* input_tensor{{details.index}} = ort_.KernelContext_GetInput(context, {{details.index}});
+    auto input_tensor{{details.index}} = ctx.GetInput({{details.index}});
     // Save shapes, DL container does not do it
-    static int64_t input{{details.index}}_shape[] = {{details.shape}};
+    static std::vector<int64_t> input{{details.index}}_shape = {{details.shape}};
 
     DLTensor dl_input{{details.index}};
     // TODO(vvchernov): device?
@@ -247,29 +234,29 @@ class TVMRunnerZeroCopy : public TVMRunnerBase {
     dl_input{{details.index}}.dtype = GetDataType(::GetInputType({{details.index}}));
     dl_input{{details.index}}.strides = nullptr;
     dl_input{{details.index}}.byte_offset = 0;
-    dl_input{{details.index}}.data = const_cast<void*>(ort_.GetTensorData<void>(input_tensor{{details.index}}));
+    dl_input{{details.index}}.data = const_cast<void*>(input_tensor{{details.index}}.GetTensorRawData());
     dl_input{{details.index}}.ndim = {{details.rank}};
-    dl_input{{details.index}}.shape = input{{details.index}}_shape;
+    dl_input{{details.index}}.shape = input{{details.index}}_shape.data();
     ort_dl_inputs.emplace_back(dl_input{{details.index}});
     {% endfor %}
     return ort_dl_inputs;
   }
 
-  std::vector<DLTensor> GetOutputDLTensors(OrtKernelContext* context) {
+  std::vector<DLTensor> GetOutputDLTensors(const Ort::KernelContext& ctx) {
     std::vector<DLTensor> ort_dl_outputs;
     {% for details in cookiecutter.outputs -%}
     // Save shapes, DL container does not do it
-    static int64_t output{{details.index}}_shape[] = {{details.shape}};
-    auto* output{{details.index}} = ort_.KernelContext_GetOutput(context, {{details.index}}, output{{details.index}}_shape, {{details.rank}});
+    static std::vector<int64_t> output{{details.index}}_shape = {{details.shape}};
+    auto output{{details.index}} = ctx.GetOutput({{details.index}}, output{{details.index}}_shape);
     // TODO(vvchernov): check output{{details.index}}->IsTensor()
     DLTensor dl_output{{details.index}};
     dl_output{{details.index}}.device = dl_device;
     dl_output{{details.index}}.dtype = GetDataType(::GetOutputType({{details.index}}));
-    dl_output{{details.index}}.data = ort_.GetTensorMutableData<void>(output{{details.index}});
+    dl_output{{details.index}}.data = output{{details.index}}.GetTensorMutableRawData();
     dl_output{{details.index}}.strides = nullptr;
     dl_output{{details.index}}.byte_offset = 0;
     dl_output{{details.index}}.ndim = {{details.rank}};
-    dl_output{{details.index}}.shape = output{{details.index}}_shape;
+    dl_output{{details.index}}.shape = output{{details.index}}_shape.data();
     ort_dl_outputs.emplace_back(dl_output{{details.index}});
     {% endfor %}
     return ort_dl_outputs;
@@ -290,14 +277,6 @@ class TVMRunnerZeroCopy : public TVMRunnerBase {
     funcs->set_outputs_func.CallPacked(tvm::runtime::TVMArgs(tvm_values.data(), tvm_type_codes.data(), num_total_args), &rv);
   }
 };
-
-std::unique_ptr<TVMRunnerBase> GetRunner(const Ort::CustomOpApi& ort, TVMFuncsPtr pfuncs, bool use_zero_copy) {
-  if (use_zero_copy) {
-    return std::make_unique<TVMRunnerZeroCopy>(ort, std::move(pfuncs));
-  } else {
-    return std::make_unique<TVMRunnerCopy>(ort, std::move(pfuncs));
-  }
-}
 
 struct TVMRuntime {
   TVMRuntime() {
@@ -381,100 +360,12 @@ struct TVMRuntime {
         .CallPacked(
             tvm::runtime::TVMArgs(init_vals.data(), codes.data(), arity), &rv);
 
-    set_input_func = vm.GetFunction("set_input", nullptr);
-    set_outputs_func = vm.GetFunction("set_outputs", nullptr);
-    get_output_func = vm.GetFunction("get_output", nullptr);
-    run_func = vm.GetFunction("invoke", nullptr);
-  }
-
-  static DLDataType GetDataType(const ONNXTensorElementDataType& type) {
-    static std::unordered_map<ONNXTensorElementDataType, DLDataType> ortTypeToDLType = {
-      {ONNX_TENSOR_ELEMENT_DATA_TYPE_FLOAT16, DLDataType{kDLFloat, 16, 1}},
-      {ONNX_TENSOR_ELEMENT_DATA_TYPE_FLOAT, DLDataType{kDLFloat, 32, 1}},
-      {ONNX_TENSOR_ELEMENT_DATA_TYPE_DOUBLE, DLDataType{kDLFloat, 64, 1}},
-      {ONNX_TENSOR_ELEMENT_DATA_TYPE_INT8, DLDataType{kDLInt, 8, 1}},
-      {ONNX_TENSOR_ELEMENT_DATA_TYPE_INT32, DLDataType{kDLInt, 32, 1}},
-      {ONNX_TENSOR_ELEMENT_DATA_TYPE_INT64, DLDataType{kDLInt, 64, 1}},
-      {ONNX_TENSOR_ELEMENT_DATA_TYPE_BOOL, DLDataType{kDLUInt, 1, 1}}
-    };
-    if(!ortTypeToDLType.count(type)) {
-      // TODO(vvchernov): implement with ORT or TVM check API
-      throw std::logic_error("Unsupported data type");
-    }
-    return ortTypeToDLType[type];
-  }
-
-  std::vector<DLTensor> GetInputDLTensors(const Ort::KernelContext& ctx) {
-    std::vector<DLTensor> ort_dl_inputs;
-    {% for details in cookiecutter.inputs -%}
-    auto input_tensor{{details.index}} = ctx.GetInput({{details.index}});
-    // Save shapes, DL container does not do it
-    static std::vector<int64_t> input{{details.index}}_shape = {{details.shape}};
-
-    DLTensor dl_input{{details.index}};
-    // TODO(vvchernov): device?
-    // auto ort_device_type = input_tensor{{details.index}}.GetTensorMemoryInfo().GetDeviceType();
-    dl_input{{details.index}}.device = dl_device;
-    dl_input{{details.index}}.dtype = GetDataType(::GetInputType({{details.index}}));
-    dl_input{{details.index}}.strides = nullptr;
-    dl_input{{details.index}}.byte_offset = 0;
-    dl_input{{details.index}}.data = const_cast<void*>(input_tensor{{details.index}}.GetTensorRawData());
-    dl_input{{details.index}}.ndim = {{details.rank}};
-    dl_input{{details.index}}.shape = input{{details.index}}_shape.data();
-    ort_dl_inputs.emplace_back(dl_input{{details.index}});
-    {% endfor %}
-    return ort_dl_inputs;
-  }
-
-  void SetInputTensors(std::vector<DLTensor>& ort_dl_inputs,
-                       const std::string& func_name) {
-    size_t num_total_args = ort_dl_inputs.size() + 1;
-    std::vector<TVMValue> tvm_in_values(num_total_args);
-    std::vector<int> tvm_in_type_codes(num_total_args);
-    tvm::runtime::TVMArgsSetter setter(tvm_in_values.data(), tvm_in_type_codes.data());
-    setter(0, func_name.c_str());
-    for (size_t k = 0; k < num_total_args - 1; ++k) {
-      setter(k+1, &ort_dl_inputs[k]);
-    }
-
-    tvm::runtime::TVMRetValue rv;
-    set_input_func.CallPacked(
-        tvm::runtime::TVMArgs(tvm_in_values.data(), tvm_in_type_codes.data(), int(num_total_args)), &rv);
-  }
-
-  std::vector<DLTensor> GetOutputDLTensors(const Ort::KernelContext& ctx) {
-    std::vector<DLTensor> ort_dl_outputs;
-    {% for details in cookiecutter.outputs -%}
-    // Save shapes, DL container does not do it
-    static std::vector<int64_t> output{{details.index}}_shape = {{details.shape}};
-    auto output{{details.index}} = ctx.GetOutput({{details.index}}, output{{details.index}}_shape);
-    // TODO(vvchernov): check output{{details.index}}->IsTensor()
-    DLTensor dl_output{{details.index}};
-    dl_output{{details.index}}.device = dl_device;
-    dl_output{{details.index}}.dtype = GetDataType(::GetOutputType({{details.index}}));
-    dl_output{{details.index}}.data = output{{details.index}}.GetTensorMutableRawData();
-    dl_output{{details.index}}.strides = nullptr;
-    dl_output{{details.index}}.byte_offset = 0;
-    dl_output{{details.index}}.ndim = {{details.rank}};
-    dl_output{{details.index}}.shape = output{{details.index}}_shape.data();
-    ort_dl_outputs.emplace_back(dl_output{{details.index}});
-    {% endfor %}
-    return ort_dl_outputs;
-  }
-
-  void LinkOutputTensors(std::vector<DLTensor>& ort_dl_outputs,
-                         const std::string& func_name) {
-    size_t num_total_args = ort_dl_outputs.size() + 1;
-    std::vector<TVMValue> tvm_values(num_total_args);
-    std::vector<int> tvm_type_codes(num_total_args);
-    tvm::runtime::TVMArgsSetter setter(tvm_values.data(), tvm_type_codes.data());
-    setter(0, func_name.c_str());
-    for (size_t k = 0; k < num_total_args - 1; ++k) {
-      setter(k+1, &ort_dl_outputs[k]);
-    }
-
-    tvm::runtime::TVMRetValue rv;
-    set_outputs_func.CallPacked(tvm::runtime::TVMArgs(tvm_values.data(), tvm_type_codes.data(), num_total_args), &rv);
+    TVMFuncsPtr funcs = std::make_unique<TVMFuncs>({
+      vm.GetFunction("set_input", nullptr),
+      vm.GetFunction("set_outputs", nullptr),
+      vm.GetFunction("invoke", nullptr)
+    });
+    runner = TVMRunnerBase::GetRunner(std::move(funcs), use_zero_copy);
   }
 
   void Compute(OrtKernelContext* context) {
@@ -486,25 +377,12 @@ struct TVMRuntime {
       constants_bound = true;
     }
 
-    // Formally we should do set_input, set_outputs and run for the same func name
-    // TODO(vvchernov): can func_name be not "main"? I have never seen such case
-    const std::string func_name = "main";
-    std::vector<DLTensor> ort_dl_inputs = GetInputDLTensors(ctx);
-    SetInputTensors(ort_dl_inputs, func_name);
-
-    std::vector<DLTensor> ort_dl_outputs = GetOutputDLTensors(ctx);
-    LinkOutputTensors(ort_dl_outputs, func_name);
-
-    // Inference
-    run_func(func_name);
+    runner->run(ctx);
   }
 
  private:
+  std::unique_ptr<TVMRunnerBase> runner;
   tvm::runtime::vm::VirtualMachine vm;
-  tvm::runtime::PackedFunc set_input_func;
-  tvm::runtime::PackedFunc set_outputs_func;
-  tvm::runtime::PackedFunc get_output_func;
-  tvm::runtime::PackedFunc run_func;
   tvm::runtime::Module exec_mod;
   tvm::runtime::ObjectPtr<tvm::runtime::vm::Executable> exec;
   TempFile model_so_file;
