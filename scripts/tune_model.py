0a542107ab7401aef023d3eec260d84319821594
Robert Kimball <bobkimball@gmail.com>
2023-03-07 17:11:03 +0000

    Start of tuning script

---
+++ scripts/tune_model.py
@@ -0,0 +1,89 @@
+import argparse
+import tempfile
+import typing
+import re
+
+import tvm
+import tvm.meta_schedule.measure_callback as measure_callback
+from tvm import meta_schedule, nd, relay
+from tvm.contrib import graph_executor
+from tvm.meta_schedule import database as ms_database
+from tvm.relay import vm
+from tvm.runtime import profiler_vm
+from tvm.runtime import vm as vm_rt
+
+import onnx
+
+
+def tune(
+    model,
+    target: tvm.target.Target,
+    axis_map: typing.Dict[str, int],
+    max_trials_global=128,
+):
+    onnx_model = onnx.load(model)
+    initializer_names = [n.name for n in onnx_model.graph.initializer]
+    input_shapes = {}
+    for i in onnx_model.graph.input:
+        if i.name not in initializer_names:
+            input_name, _, dtype, axis_names = relay.frontend.onnx.get_info(i)
+            shape = []
+            for val in axis_names:
+                if val in axis_map.keys():
+                    val = axis_map[val]
+                shape.append(val)
+            input_shapes[input_name] = shape
+
+    mod, params = relay.frontend.from_onnx(
+        onnx_model, shape=input_shapes, freeze_params=True
+    )
+
+    with tempfile.TemporaryDirectory() as work_dir:
+        with target:
+            database = meta_schedule.relay_integration.tune_relay(
+                mod,
+                params,
+                target,
+                work_dir=work_dir,
+                max_trials_global=max_trials_global,
+            )
+
+    print("Tuning Time:")
+    return database
+
+
+def main():  # pragma: no cover
+    parser = argparse.ArgumentParser(description="Tune an onnx model with TVM.")
+    parser.add_argument(
+        "--model",
+        required=True,
+        help="Source model in .onnx format",
+    )
+    parser.add_argument(
+        "--output",
+        required=True,
+        help="Output file in tvm in onnx format.",
+    )
+    parser.add_argument(
+        "--axis-size",
+        required=False,
+        type=str,
+        help="Define a static value for a named dynamic axis",
+    )
+    args = parser.parse_args()
+
+    axis_map = {}
+    if args.axis_size:
+        m = re.match("(.+)=(\d+)", args.axis_size)
+        axis_map[m[1]] = int(m[2])
+    print(axis_map)
+
+    tune(
+        model=args.model,
+        axis_map=axis_map,
+        target=tvm.target.Target("llvm -num-cores 8"),
+    )
+
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
