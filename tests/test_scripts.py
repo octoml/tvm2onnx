fd48eb4d58266e7064bc2e0bb5a3ae3bc11d0f5b
Robert Kimball <bobkimball@gmail.com>
2023-03-13 18:35:47 +0000

    lint

--- tests/test_scripts.py
+++ tests/test_scripts.py
@@ -12,10 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
 import subprocess
 import sys
 import tempfile
-import os
 
 
 def test_package_and_benchmark():
